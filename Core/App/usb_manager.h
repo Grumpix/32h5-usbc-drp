091aa2f038f2f3490aa9b3c3da0d023e5a756539
Grumpix <d.novak9624@gmail.com>
2026-06-30 15:52:30 +0200

    backup2

--- Core/App/usb_manager.h
+++ Core/App/usb_manager.h
@@ -1,13 +1,8 @@
 #pragma once
 #include <stdint.h>
-#include "drp_fsm.h"
 
-
-void usb_manager_init(void);
-void usb_manager_task(void);
-
-void usb_manager_start_device(void);
-void usb_manager_start_host(void);
-void usb_manager_stop(void);
-
-usb_mode_t usb_manager_get_mode(void);
+typedef enum {
+    USB_MODE_NONE = 0,
+    USB_MODE_DEVICE,
+    USB_MODE_HOST
+} usb_mode_t;
